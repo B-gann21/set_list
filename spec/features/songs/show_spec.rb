3360d61736f996b3e463350310dcf02bd8a192aa
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-22 19:28:53 -0600

    View: Songs#show shows the title and artist

--- spec/features/songs/show_spec.rb
+++ spec/features/songs/show_spec.rb
@@ -11,5 +11,12 @@ RSpec.describe 'the songs show page' do
     expect(page).to_not have_content(song2.title)
   end
 
-  it 'shows the name of the artist who wrote the song'
+  it 'shows the name of the artist who wrote the song' do
+    artist = Artist.create!(name: "Bippity Boop")
+    song = artist.songs.create!(title: "I really like you", length: 208, play_count: 230)
+    song2 = artist.songs.create!(title: "Skibbity Bop", length: 2000, play_count: 230000)
+    visit "/songs/#{song.id}"
+
+    expect(page).to have_content(artist.name)
+  end
 end
