38ad821c9867966779648e3cbd4a3b6a024928b4
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-27 08:40:50 -0600

    Feat: you can edit an artist

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get '/artists/new', to: "artists#new"
   post '/artists', to: "artists#create"
   get '/artists/:id/edit', to: "artists#edit"
+  patch '/artists/:id', to: "artists#update"
 end
